6508915336fe6210ae4fc5b165a0f9572f4726e6
Juha Kovanen <juha.kovanen@gmail.com>
2020-12-11 22:26:34 +0200

    default port 3000 changed to ~particl green

--- src/core/Bootstrap.ts
+++ src/core/Bootstrap.ts
@@ -26,7 +26,7 @@ export class Bootstrap {
 
     public defineExpressApp(app: express.Application): express.Application {
         app.set('host', process.env.APP_HOST);
-        app.set('port', Server.normalizePort(process.env.APP_PORT || '3000'));
+        app.set('port', Server.normalizePort(process.env.APP_PORT || '45492'));
         return app;
     }
 
